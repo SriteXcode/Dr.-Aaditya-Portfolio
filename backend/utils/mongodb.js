fbbeb5be56555423cf18d5b93958288fd3250cc8
SriteXcode <riteshgangster420@gmail.com>
2025-09-01 15:09:02 +0530

    added backend an fix bugs

--- backend/utils/mongodb.js
+++ backend/utils/mongodb.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB connected");
+   await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
     process.exit(1); // stop app if DB fails
